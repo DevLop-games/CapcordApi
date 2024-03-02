9c75faef89825aa42b0e4651e3b70d8167241bf8
DevLop-games <143568491+DevLop-games@users.noreply.github.com>
2024-03-02 13:04:33 -0600

    Add files via upload

---
+++ CapcordApi/main.py
@@ -0,0 +1,38 @@
+import flask
+import sqlite3
+import random,string
+
+app = flask.Flask("__name__")
+
+con = sqlite3.connect("CapcordApi.db")
+cur = con.cursor()
+
+def GenerateUserId():
+    chars = string.digits + string.ascii_lowercase
+
+    return "".join(random.choice(chars) for i in range(len(chars)-1))
+
+@app.route("/",methods=["Get"])
+def Home():
+    return "Home page"
+
+@app.route("/CreateAccount",methods=["Get"])
+def CreateAccount(Username,Password):
+    NewId = GenerateUserId()
+    
+    cur.execute(f"""INSERT INTO Login VALUES
+                ({Username},{Password},{NewId})
+                """)
+    
+@app.route("/GetAccountInfo",methods=["Get"])
+def GetAccountInfo():
+    User = cur.execute("SELECT Username FROM Login")
+    Pass = cur.execute("SELECT Password FROM Login")
+    Id = cur.execute("SELECT UserId FROM Login")
+    
+    return User.fetchall(),Pass.fetchall(),Id.fetchall()
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+cur.close()
